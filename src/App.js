51f5721056d70269ce2bb85ff4df408c92fb8a44
johnpospisil <audiofreak7@gmail.com>
2019-08-31 13:31:58 -0700

    Connect the React Components to the main App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>Hello, from the app!</h3>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
       </React.Fragment>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Navbar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h3>Hello, from the Navbar!</h3>
+      </div>
+    );
   }
 }
