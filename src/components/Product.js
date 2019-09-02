7f6f0896791cc9c0aa159e7b0b7af2de2d9f2501
johnpospisil <audiofreak7@gmail.com>
2019-09-02 13:04:33 -0700

    Add product price and name to each product in the ProductList

--- src/components/Product.js
+++ src/components/Product.js
@@ -34,6 +34,14 @@ export default class Product extends Component {
               )}
             </button>
           </div>
+          {/* Card Footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0 ">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
+          </div>
         </div>
       </ProductWrapper>
     );
